3a0d00906ef6d0b5921ba94247509161545607c1
lisga <lsg@rrtimes.com>
2017-07-28 09:57:54 +0800

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const config = require('./config')
+const api = require('./api')
+
+module.exports = {
+    name: "mk-service-auth",
+    version: "",
+    description: "",
+    author: "lsg",
+    config,
+    apiRootUrl: false,
+    api,
+}
