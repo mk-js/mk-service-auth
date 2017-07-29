e19658dd38c099d3c38210b9bb3bab3e29c57b97
lsg <lsg@rrtimes.com>
2017-07-29 14:28:29 +0800

    apiRootUrl: false,不绑定url

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const config = require('./config')
 const api = require('./api')
 
 module.exports = {
+    apiRootUrl: false,
     name: "mk-service-auth",
     version: "",
     description: "",
     author: "lsg",
     config,
-    apiRootUrl: false,
     api,
 }
