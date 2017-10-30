1ca42bc7a7ea932fd67e6ce09b750e4d7ff0705c
lsg <lsg@rrtimes.com>
2017-10-30 13:20:15 +0800

    update

--- api.js
+++ api.js
@@ -1,21 +1,67 @@
+const jwt = require('jsonwebtoken');
+let config
 
-function config(options) {
-    Object.assign(current, options)
-    current.secret = new Buffer(options.key , 'base64');
-    current.excludeUrls = {};
-    current.exclude.forEach(i => current.excludeUrls[i] = true)
-    if(current.init)current.init();
-    return current;
+const api = {
+    _init: (current) => {
+        config = current
+        let array = config.server.interceptors || [];
+        if (array.filter(a => a == interceptor) == 0) {
+            array.push(interceptor)
+        }
+        config.server.interceptors = array
+    }
 }
 
-var current = {
-    key: 'token/key',
-    tokenKeys: null,
-    exclude: [], 
-    secret: null,
-    expire: 5 * 24 * 60 * 60 , //5 days, seconds
+
+function interceptor(ctx) {
+    //向上下文中增加setToken方法和token对象。
+    ctx.setToken = (obj) => {
+        ctx.resBody.token = encodeToken(obj);
+        return ctx;
+    };
+    ctx.token = {};
+
+    let clientToken = ctx.request.headers.token || ctx.request.payload && ctx.request.payload.token || ctx.request.url.query.token;
+
+    try {
+        ctx.token = decodeToken(clientToken);
+    } catch (error) {
+        let { excludeUrls, apiRootUrl } = config;
+        if (excludeUrls[apiRootUrl + "/*"] || excludeUrls[ctx.apiUrl]) return true;
+
+        ctx.error(config.errorObj);
+        return false;
+    }
+    return true;
 }
 
-module.exports = Object.assign(config, {
-    current,
-})
+function encodeToken(obj) {
+    let { secret, expire, tokenKeys } = config;
+    let arr = [];
+
+    if (!Array.isArray(obj) && Array.isArray(tokenKeys)) {
+        tokenKeys.forEach((k, i) => arr[i] = obj[k])
+    } else {
+        arr = obj;
+    }
+    let sub = JSON.stringify(arr);
+    let exp = Math.floor(Date.now() / 1000) + expire;
+    let str = jwt.sign({ sub, exp }, secret, { algorithm: 'HS512' });
+    return str;
+}
+
+function decodeToken(str) {
+    if (!str) throw ({ code: 10, message: "empty token" });
+    let { secret, tokenKeys } = config;
+
+    let json = jwt.verify(str, secret, { algorithms: ['HS512'] });
+    let obj = JSON.parse(json.sub)
+    let token = obj
+    if (Array.isArray(obj) && Array.isArray(tokenKeys)) {
+        token = {}
+        tokenKeys.forEach((k, i) => token[k] = obj[i])
+    }
+    return token;
+}
+
+module.exports = api
